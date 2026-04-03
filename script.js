3458fda21505dd4854bd48534a99d6f805d6f67b
Maydisil <maydsondinael@hotmail.com>
2026-04-03 16:08:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -259,7 +259,8 @@ async function carregarAnuncios() {
   const aviso = document.getElementById("semResultados");
   if (aviso) aviso.remove();
   // ⏳ LOADING
-  container.innerHTML = '<div class="loading">Carregando anúncios...</div>';
+  container.innerHTML = '<div class="loading">Carregando anúncios...</div>
+	  <div class="spinner"></div>';
   try {
     const resposta = await fetch(`${SCRIPT_SITE}?funcao=listarAnuncios`);
     if (!resposta.ok) {
